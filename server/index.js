135e9da4abd7e86adef26dd4fd7de84568301433
paul schmidt <schmidt@heitaria.ch>
2025-12-17 08:08:19 +0100

    chore: push pending changes

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use('/api/hotels', hotelRoutes);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
